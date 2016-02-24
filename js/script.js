3f9d3820838a76a047c38dfc315375b55378395c
Serezha <csserezha@gmail.com>
2016-02-24 13:59:28 +0300

    REACT ADDED

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-﻿
+﻿var wysiAdd = React.createClass({
+	render: function() {
+		return (
+			<div className="testClass">
+			<h1>TEST</h1>
+			</div>
+		)
+	}
+});
+
+ReactDOM.render(<wysiAdd />, document.body);
